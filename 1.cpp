223db172aa0ecb4cdf145f5eec6e55650ed5355b
Rohit <Rohit>
2022-11-29 12:53:38 +0530

    Added Sum Function

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int add(int a,int b){
     return a+b;
 }
+
+int add(int a,int b,int c){
+    return a+b+c;
+}
+
+int add(int a,int b,int c,int d){
+    return a+b+c+d;
+}
 int main()
 {
     return 0;
