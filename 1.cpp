cb8b8bd797ce7053def03a5d208e2237018ba1e1
Rohit <Rohit>
2022-11-29 12:55:30 +0530

    Added Sub Function

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,15 @@ using namespace std;
 int sub(int a,int b){
     return a-b;
 }
+
+int sub(int a,int b,int c){
+    return a-b-c;
+}
+
+
+int sub(int a,int b,int c,int d){
+    return a-b-c-d;
+}
 int main()
 {
     return 0;
